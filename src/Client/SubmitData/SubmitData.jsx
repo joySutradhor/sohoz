8abfdd76263407304eeaaa6a72968e2d92e3624a
joySutradhor <joysutradhorcmt@gmail.com>
2023-09-27 01:50:14 +0600

    react hook form updated

--- src/Client/SubmitData/SubmitData.jsx
+++ src/Client/SubmitData/SubmitData.jsx
@@ -1,11 +1,6 @@
 /* eslint-disable react/jsx-no-duplicate-props */
 import { useForm } from "react-hook-form"
 import TextField from '@mui/material/TextField';
-import Select from '@mui/material/Select';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import { useState } from "react";
-import FormControl from '@mui/material/FormControl';
 // import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 export default function SubmitData() {
@@ -16,16 +11,10 @@ export default function SubmitData() {
         formState: { errors },
     } = useForm()
 
-
     const onSubmit = (data) => console.log(data)
 
-
     console.log(watch("example")) // watch input value by passing the name of it
-    const [age, setAge] = useState('');
 
-    const handleChange = (event) => {
-        setAge(event.target.value);
-    };
 
     return (
         <div className="grid  justify-center items-center h-screen bg-slate-50">
@@ -33,43 +22,38 @@ export default function SubmitData() {
 
             <div className="shadow-md mx-2 bg-white rounded-md">
                 <h1 className="font-bold text-2xl text-gray-600 text-center py-5 ">Submit Customer Data</h1>
-                <form  onSubmit={handleSubmit(onSubmit)}>
+                <form onSubmit={handleSubmit(onSubmit)}>
 
                     <div className="grid grid-cols-2 gap-5 px-5" >
-                    <TextField id="outlined-read-only-input" {...register("Id")} defaultValue={2} label="ID" InputProps={{
-                        readOnly: true,
-                    }} variant="standard" />
-                    <TextField id="standard-basic" {...register("name")} label=" Name" variant="standard" />
-                    <TextField id="standard-basic" {...register("phone")} label="Phone" variant="standard" />
-                    <TextField id="standard-basic" {...register("need")} label="Need" variant="standard" />
-                    {/* <DatePicker {...register("date")} label="Basic date picker" /> */}
-                    <TextField id="standard-basic" {...register("endDate")} label="End Date" variant="standard" />
-                    <TextField id="standard-basic" {...register("diller")} label="Diller Price" variant="standard" />
-                    <TextField id="standard-basic" {...register("seller")} label="Seller Price" variant="standard" />
-                    <TextField id="standard-basic" {...register("profit")} label="Profit" variant="standard" />
-                    <TextField id="standard-basic" {...register("address")} label="Address" variant="standard" />
-                    <TextField id="standard-basic" {...register("addressCode")} label="Address Code" variant="standard" />
-                    <TextField id="standard-basic" {...register("cost")} label="Cost" variant="standard" />
-                    <TextField id="standard-basic" {...register("cost")} label="Cost" variant="standard" />
-                    <TextField id="standard-basic" {...register("costDetails")} label="Cost Details" variant="standard" />
-                    <FormControl variant="standard" sx={{ minWidth: 120 }}>
-                        <InputLabel id="demo-simple-select-standard-label">Age</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-standard-label"
-                            id="demo-simple-select-standard"
-                            value={age}
-                            onChange={handleChange}
-                            label="Age"
+                        <TextField id="outlined-read-only-input" {...register("customerId")} defaultValue={2} label="Customer ID" InputProps={{
+                            readOnly: true,
+                        }} variant="standard" />
+                        <TextField id="outlined-read-only-input" {...register("riderId")} defaultValue={2} label="Rider ID" InputProps={{
+                            readOnly: true,
+                        }} variant="standard" />
+                        <TextField id="standard-basic" {...register("name")} label=" Name" variant="standard" />
+                        
+                        <TextField id="standard-basic" {...register("phone")} label="Phone" variant="standard" />
+                        <TextField id="standard-basic" {...register("need")} label="Monthly Need" variant="standard" />
+                        <TextField id="standard-basic" {...register("brand")} label="Brand Name" variant="standard" />
 
-                        >
-                            <MenuItem value="">
-                                <em>None</em>
-                            </MenuItem>
-                            <MenuItem value={10}>Ten</MenuItem>
-                            <MenuItem value={20}>Twenty</MenuItem>
-                            <MenuItem value={30}>Thirty</MenuItem>
-                        </Select>
-                    </FormControl>
+                        {/* <DatePicker {...register("date")} label="Basic date picker" /> */}
+                        <div className="pt-1">
+                            <p className="text-[14px] p-0 text-gray-600">Start Date</p>
+                            <input type="date" name="StartDate" id="" className="outline-none  border-gray-400 border-b-2 text-gray-500" />
+                        </div>
+                        <div className="pt-1">
+                            <p className="text-[14px] text-gray-600">End Date</p>
+                            <input type="date" name="EndDate" id="" className="outline-none border-gray-400 border-b-2 text-gray-500" />
+                        </div>
+                        <TextField id="standard-basic" {...register("diller")} label="Diller Price" variant="standard" />
+                        <TextField id="standard-basic" {...register("seller")} label="Seller Price" variant="standard" />
+                        <TextField id="standard-basic" {...register("address")} label="Address" variant="standard" />
+                        <TextField id="standard-basic" {...register("addressCode")} label="Address Code" variant="standard" />
+                        <TextField id="standard-basic" {...register("cost")} label="Cost" variant="standard" />
+                        <TextField id="standard-basic" {...register("costDetails")} label="Cost Details" variant="standard" />
+                        
+                        
                     </div>
 
 
