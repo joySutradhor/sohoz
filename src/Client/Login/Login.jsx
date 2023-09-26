4e3cad45c061390005372d8eedebac96d736ce03
joySutradhor <joysutradhorcmt@gmail.com>
2023-09-26 12:19:26 +0600

    button updated

--- src/Client/Login/Login.jsx
+++ src/Client/Login/Login.jsx
@@ -50,7 +50,7 @@ export default function Login() {
                     />
                 </div>
                 <div className='flex flex-col '>
-                    <TextField id="standard-basic" label="User ID" variant="standard" />
+                    <div className='pb-5'><TextField id="standard-basic" label="User ID" variant="standard" /></div>
                     <FormControl variant="standard">
                         <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
                         <Input
@@ -71,9 +71,8 @@ export default function Login() {
                     </FormControl>
                     <div className='flex justify-center py-8'>
                         <Button onClick={()=> fire()}>
-                            <Link to="/submitData" className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-slate-100 shadow-md  rounded hover:bg-white group">
-                                <span className="w-48 h-48 rounded rotate-[-35deg] bg-green-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-                                <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">Login Now</span>
+                            <Link to="/submitData" className=" text-white  px-6 py-3  font-medium  bg-green-600 shadow-md  rounded ">
+                           Login Now
                             </Link>
                         </Button>
                     </div>
