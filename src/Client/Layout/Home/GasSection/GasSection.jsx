be6719781620009d0247a8bbb428faadf4b66df5
joySutradhor <joysutradhorcmt@gmail.com>
2023-10-03 04:29:29 +0600

    card render from back end

--- src/Client/Layout/Home/GasSection/GasSection.jsx
+++ src/Client/Layout/Home/GasSection/GasSection.jsx
@@ -1,8 +1,53 @@
+import { useQuery } from "@tanstack/react-query";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const GasSection = () => {
+    const {  data  } = useQuery({
+        queryKey: ['cylinders'],
+        queryFn: async () => {
+            const response = await fetch('http://localhost:5000/cylinders')
+            return response.json()
+        },
+    })
+    console.log(data)
     return (
-        <div className="py-6 px-4">
-            <h1 className="text-[#051441] font-poppins text-lg font-bold text-center">Our Services </h1>
+        <div className="py-6 px-2">
+            <h1 className="text-[#5a6177] font-poppins text-lg font-bold text-center">Our Services </h1>
+            <div>
+                {
+                    data?.map((item) => <div className="grid pt-5"
+                    
+                    key={item._id}
+                    >
+                        <Card sx={{ maxWidth: 345 }}>
+                            <CardMedia
+                                component="img"
+                                alt="green iguana"
+                                height="140"
+                                image={item.img}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {item.name} - {item.weight}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Share</Button>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
