f7723a540ee3c5c0d798039cfa61229759c1e17d
joySutradhor <joysutradhorcmt@gmail.com>
2023-10-03 04:49:13 +0600

    buy button added

--- src/Client/Layout/Home/GasSection/GasSection.jsx
+++ src/Client/Layout/Home/GasSection/GasSection.jsx
@@ -3,11 +3,12 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 const GasSection = () => {
-    const {  data  } = useQuery({
+    const { data } = useQuery({
         queryKey: ['cylinders'],
         queryFn: async () => {
             const response = await fetch('http://localhost:5000/cylinders')
@@ -18,11 +19,11 @@ const GasSection = () => {
     return (
         <div className="py-6 px-2">
             <h1 className="text-[#5a6177] font-poppins text-lg font-bold text-center">Our Services </h1>
-            <div>
+            <div className="pb-20">
                 {
                     data?.map((item) => <div className="grid pt-5"
-                    
-                    key={item._id}
+
+                        key={item._id}
                     >
                         <Card sx={{ maxWidth: 345 }}>
                             <CardMedia
@@ -32,17 +33,24 @@ const GasSection = () => {
                                 image={item.img}
                             />
                             <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
+                                <Typography gutterBottom variant="h6" component="div">
                                     {item.name} - {item.weight}
                                 </Typography>
                                 <Typography variant="body2" color="text.secondary">
-                                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                                    species, ranging across all continents except Antarctica
+                                    {item.desc}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Price : {item.price} Taka
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Devlivery : {item.delivery}
                                 </Typography>
                             </CardContent>
-                            <CardActions>
-                                <Button size="small">Share</Button>
-                                <Button size="small">Learn More</Button>
+                            <CardActions className="flex justify-end mr-4 mb-2">
+                                <Button variant="outlined" startIcon={<AddShoppingCartIcon />}>
+                                    Buy
+                                </Button>
+
                             </CardActions>
                         </Card>
                     </div>)
