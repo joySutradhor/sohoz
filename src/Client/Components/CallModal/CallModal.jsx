d9cb3bd423b518f5b55c4ed7dfa7f499977659c6
joySutradhor <joysutradhorcmt@gmail.com>
2023-10-04 04:23:15 +0600

    add modal onclick call button

---
+++ src/Client/Components/CallModal/CallModal.jsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Gif from "../../../../public/call.gif"
+
+export default function CallModal() {
+  const [open, setOpen] = React.useState(false);
+
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleClickOpen}>
+      <img  src={Gif} alt="" width="35" height="50" />
+      </Button>
+      <Dialog
+
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle id="responsive-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Let Google help apps determine location. This means sending anonymous
+            location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Disagree
+          </Button>
+          <Button onClick={handleClose} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
--- src/Client/Shared/Footer.jsx
+++ src/Client/Shared/Footer.jsx
@@ -5,16 +5,14 @@ import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-// import Paper from '@mui/material/Paper';
 import Fab from '@mui/material/Fab';
 import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
-// import CallOutlinedIcon from '@material-ui/icons/CallOutlined';
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import { Link } from 'react-router-dom';
 import Badge from '@mui/material/Badge';
 import DirectionsBikeOutlinedIcon from '@material-ui/icons/DirectionsBikeOutlined';
 import PhotoLibraryOutlinedIcon from '@material-ui/icons/PhotoLibraryOutlined';
-import Gif from "../../../public/call.gif"
+import CallModal from '../Components/CallModal/CallModal';
 
 
 
@@ -30,17 +28,11 @@ const StyledFab = styled(Fab)({
 
 export default function Footer() {
 
-  // to add a modal 
-  const hello = () => {
-    console.log ("hello click")
-  }
 
   return (
     <React.Fragment>
       <CssBaseline />
-      {/* <Paper square sx={{ pb: '50px' }}>
 
-      </Paper> */}
       <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
         {/* Render all menu icon  */} 
         {/* Gap 4 means space vertical in menu icon  */}
@@ -52,8 +44,7 @@ export default function Footer() {
             <Link to="/"> <PhotoLibraryOutlinedIcon></PhotoLibraryOutlinedIcon> </Link>
           </IconButton>
           <StyledFab  aria-label="add">
-            {/* <CallOutlinedIcon></CallOutlinedIcon> */}
-            <img onClick={hello} src={Gif} alt="" width="35" height="50" />
+            <CallModal></CallModal>
           </StyledFab>
           <Box sx={{ flexGrow: 1 }} />
           <IconButton color="inherit">
