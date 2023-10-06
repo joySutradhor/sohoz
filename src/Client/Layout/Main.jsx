b7557098ea2d551f7ae389b967c87f3ab88691af
joySutradhor <joysutradhorcmt@gmail.com>
2023-10-06 22:50:29 +0600

    hide header foooter in login and register page

--- src/Client/Layout/Main.jsx
+++ src/Client/Layout/Main.jsx
@@ -1,18 +1,25 @@
 import Footer from "../Shared/Footer";
 import Header from "../Shared/Header";
-import { Outlet } from 'react-router-dom';
+// import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 const Main = () => {
-    // const noHeaderFooter = location.pathname.includes('login')||location.pathname.includes('register')||location.pathname.includes('updateProfile') 
+    const location = useLocation();
+  
+    // Define an array of pathnames where you want to hide the Header and Footer.
+    const pathsToHideHeaderFooter = ['/login', '/register', '/updateProfile'];
+  
+    // Check if the current pathname is in the array of paths to hide Header and Footer.
+    const shouldHideHeaderFooter = pathsToHideHeaderFooter.includes(location.pathname);
+  
     return (
-        <div>
-            
-            <Header></Header>
-            <Outlet />
-            <Footer></Footer>
-            
-        </div>
+      <div>
+        {!shouldHideHeaderFooter && <Header />}
+        <Outlet />
+        {!shouldHideHeaderFooter && <Footer />}
+      </div>
     );
-};
-
-export default Main;
+  };
+  
+  export default Main;
+  
