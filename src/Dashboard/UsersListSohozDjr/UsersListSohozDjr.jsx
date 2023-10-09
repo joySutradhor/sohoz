ecc1257507a0027ff375eeeb52f64b078253e084
joySutradhor <joysutradhorcmt@gmail.com>
2023-10-10 05:32:41 +0600

    seach button fixed

--- src/Dashboard/UsersListSohozDjr/UsersListSohozDjr.jsx
+++ src/Dashboard/UsersListSohozDjr/UsersListSohozDjr.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Paper from '@mui/material/Paper';
 import List from '@mui/material/List';
@@ -9,67 +9,60 @@ import ListSubheader from '@mui/material/ListSubheader';
 import Avatar from '@mui/material/Avatar';
 import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
 import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
-import SearchButton from '../../Client/Components/SearchButton/SearchButton';
-import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
-// import { Swal } from 'sweetalert2';
 import Swal from 'sweetalert2';
-
-
-
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
+import { Link } from 'react-router-dom';
 
 export default function UsersListSohozDjr() {
     const [snackbarOpen, setSnackbarOpen] = useState(false);
     const [snackbarMessage, setSnackbarMessage] = useState('');
-    const [snackbarSeverity, setSnackbarSeverity] = useState('success'); // Set the default severity
-
+    const [snackbarSeverity, setSnackbarSeverity] = useState('success');
+    const [emailSearch, setEmailSearch] = useState('');
 
     const { data, refetch } = useQuery(["users"], async () => {
-        const res = await fetch("http://localhost:5000/users")
-        return res.json()
+        const res = await fetch("http://localhost:5000/users");
+        return res.json();
+    });
 
-    })
-    console.log(data)
     const handleMakeAdmin = (_id, name) => {
-        console.log(_id)
         fetch(`http://localhost:5000/users/admin/${_id}`, {
-            method: "PATCH"
+            method: "PATCH",
         })
-
-            .then(res => res.json())
-            .then(Userdata => {
-                refetch()
-                console.log(Userdata)
+            .then((res) => res.json())
+            .then((Userdata) => {
+                refetch();
                 if (Userdata.modifiedCount) {
                     setSnackbarMessage(`${name} is now an admin`);
                     setSnackbarSeverity('success');
                     setSnackbarOpen(true);
                 }
-            })
-    }
-    const handleMakeManager = (_id, name) => {
-        console.log(_id)
-        fetch(`http://localhost:5000/users/manager/${_id}`, {
-            method: "PATCH"
-        })
+            });
+    };
 
-            .then(res => res.json())
-            .then(Userdata => {
-                refetch()
-                console.log(Userdata)
+    const handleMakeManager = (_id, name) => {
+        fetch(`http://localhost:5000/users/manager/${_id}`, {
+            method: "PATCH",
+        })
+            .then((res) => res.json())
+            .then((Userdata) => {
+                refetch();
                 if (Userdata.modifiedCount) {
                     setSnackbarMessage(`${name} is now a manager`);
                     setSnackbarSeverity('success');
                     setSnackbarOpen(true);
                 }
-            })
-    }
+            });
+    };
 
     const handleDelete = (_id, name) => {
-        console.log(_id, name);
         Swal.fire({
             title: 'Are you sure?',
             icon: 'warning',
@@ -96,11 +89,17 @@ export default function UsersListSohozDjr() {
         });
     };
 
-
-
-
     const handleCloseSnackbar = () => {
         setSnackbarOpen(false);
+    };
+
+    // Filter users based on email search
+    const filteredUsers = data?.filter((user) =>
+        user.email.toLowerCase().includes(emailSearch.toLowerCase())
+    );
+
+    const handleEmailSearch = (event) => {
+        setEmailSearch(event.target.value);
     };
 
     return (
@@ -109,11 +108,39 @@ export default function UsersListSohozDjr() {
             <Paper square sx={{ backgroundColor: "#F3F6FC", height: "100vh", border: "none" }} >
 
                 <ListSubheader sx={{ width: "full", padding: "0", paddingTop: "0" }}>
-                    <SearchButton></SearchButton>
+
                 </ListSubheader>
 
+                <div className="mb-2">
+
+                    <Paper
+                        component="form"
+                        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: "full" }}
+                    >
+                        <Link to="/dashboardHomeSohozDjr">
+                            <IconButton sx={{ p: '10px' }} aria-label="menu">
+                                <ArrowBackIosNewOutlinedIcon />
+                            </IconButton>
+                        </Link>
+                        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+                        <InputBase
+                            value={emailSearch}
+                            onChange={handleEmailSearch}
+                            sx={{ ml: 1, flex: 1 }}
+                            placeholder="Search Users"
+                            inputProps={{ 'aria-label': 'search Users' }}
+                        />
+                        {/* <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" /> */}
+                        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                            <SearchIcon />
+                        </IconButton>
+                    </Paper>
+
+
+                </div>
+
                 <List sx={{ mb: 0, paddingTop: "0", }}>
-                    {data?.map(({ email, name, url, _id, role, phone }) => (
+                    {filteredUsers?.map(({ email, name, url, _id, role, phone }) => (
                         <div key={_id}>
                             <ListItem className='bg-white shadow-sm rounded-sm  my-2  '>
                                 <ListItemAvatar>
@@ -125,7 +152,6 @@ export default function UsersListSohozDjr() {
                                         <span className='text-gray-400 text-sm'>{role}</span>
                                     </span>
                                 } secondary={
-
                                     <span className='text-gray-400 '>
                                         <span className='pt-1 flex flex-col'>{email}</span>
                                         <span >{phone}</span>
